bb7df3a8c4be20ced6c309f36fdccc6a697c256b
PongpornFLK <pongporn.yt2910@gmail.com>
2025-03-16 14:56:49 +0700

    Refactor Streamlit application structure; remove old main and sidebar pages, add new overview page, and update navigation.

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-main_page = st.Page(page = "pages/main.py"  , title = "Overview", icon = ":material/play_arrow:", default=True)
+
+main_page = st.Page(page = "pages/overview.py"  , title = "Pongporn Yampradit", icon = ":material/play_arrow:", default=True)
 guideline_machine = st.Page(page = "pages/guideline_machine.py"  , title = "Guideline Machine Learning", icon = ":material/play_arrow:")
 guideline_neural = st.Page(page = "pages/guideline_neural.py"  , title = "Guideline Neural Network", icon = ":material/play_arrow:")
 machine_page = st.Page(page = "pages/machine.py"  , title = "Machine Model", icon = ":material/play_arrow:")
 neural_page = st.Page(page = "pages/neural.py"  , title = "Neural Model", icon = ":material/play_arrow:")
 
-# navigation
-pg = st.navigation({ "Intelligence System" : [main_page,machine_page, neural_page] ,
-                     "Guideline / Reporting" : [guideline_machine,guideline_neural] ,
+pg = st.navigation({ "🧑🏻‍💻 Student Info" : [main_page] , 
+                     "📌 Model" : [machine_page, neural_page] ,
+                     "📌 Guideline / Reporting" : [guideline_machine,guideline_neural] ,
                   })
 pg.run() 
---
+++ pages/overview.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+
+# แสดง Title
+st.title("🧑🏻‍💻 Pongporn Yampradit")
+st.subheader("Student ID: 6504062620078.    Section: 1.")
+st.markdown(
+    '<a href="https://github.com/PongpornFLK/Intelligence-System" target="_blank" style="text-decoration: none;"> 👉🏻 <strong>My Github</strong></a>',
+    unsafe_allow_html=True
+)
+
+
+
+
+    
+
+
