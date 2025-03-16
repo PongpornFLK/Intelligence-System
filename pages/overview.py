4a94b6c45a7195f295bb63d1054db3e57259fd38
PongpornFLK <pongporn.yt2910@gmail.com>
2025-03-16 15:28:12 +0700

    Add personal image to overview page with caption

--- pages/overview.py
+++ pages/overview.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 # แสดง Title
 st.title("🧑🏻‍💻 Pongporn Yampradit")
+st.image("assets/me.jpg" , width = 500, caption = "Pongporn Yampradit")
 st.subheader("Student ID: 6504062620078.    Section: 1.")
 st.markdown(
     '<a href="https://github.com/PongpornFLK/Intelligence-System" target="_blank" style="text-decoration: none;"> 👉🏻 <strong>My Github</strong></a>',
